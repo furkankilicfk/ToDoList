c90c76fb65f51a146ded5ea032db0ad6e613aa95
furkankilicfk <78054012+furkankilicfk@users.noreply.github.com>
2021-05-07 19:36:51 +0300

    Update todolist.js

--- todolist/js/todolist.js
+++ todolist/js/todolist.js
@@ -1,61 +1,51 @@
-let items = ["3 Litre Su İç", "Ödevleri Yap", "En Az 3 Saat Kodlama Yap", "Yemek Yap", "50 Sayfa Kitap Oku"]
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
 
-let list = document.querySelector('#list')
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
 
-items.forEach(item => {
-
-    let li = document.createElement('li')
-    let t = document.createTextNode(item)
-    li.appendChild(t)
-
-    list.appendChild(li)
-
-    let span = document.createElement('span')
-    let text = document.createTextNode('*')
-    span.className = 'close'
-    span.appendChild(text)
-    li.appendChild(span)
-    
-    span.addEventListener('click', function(){
-        let li = this.parentElement;
-        li.style.display = 'none' }
-    )
-} )
-
-
-let ulList = document.querySelector("ul");
-ulList.addEventListener("click", function (item) {
-    if (item.target.tagName = 'li') {
-      item.target.classList.toggle("checked");
-    }
-  },
-  false
-);
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
 
 function newElement() {
-    var li = document.createElement("li")
-    var inputValue = document.getElementById("task").value
-    var t = document.createTextNode(inputValue)
-    li.appendChild(t)
-    if (inputValue === "" || inputValue.replace(/^\s+|\s+$/g, "").length == 0) {
-      $(".error").toast("show")
-    } else {
-      $(".success").toast("show")
-      document.getElementById("list").appendChild(li)
-    }
-    
-document.getElementById("task").value = ""
-     
-var span = document.createElement("SPAN");
-var txt = document.createTextNode("*");
-span.className = "close";
-span.appendChild(txt);
-li.appendChild(span);
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write SOMETHING!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
 
-span.addEventListener('click', function(){
-    let li = this.parentElement;
-    li.style.display = 'none' 
-      
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
     }
-  
-) }
+  }
+}
